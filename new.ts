de0e9222ed72a8e5dea1c043c981a73ca6de3014
Erum Sikander <mrsikanderghouri@gmail.com>
2024-07-21 04:48:29 +0500

    done

---
+++ new.ts
@@ -0,0 +1,28 @@
+
+
+// Aliased Object Type
+
+type ITManager = 
+{
+    name : string,
+    position : string,
+    salary : number,
+    experience : number,
+    city : string,
+}
+
+let Employee : ITManager = 
+{
+    name : "Erum Sikander",
+    position : "Manager",
+    salary : 500000,
+    experience : 5,
+    city : "Karachi"
+}
+
+console.log(Employee.name + " " + Employee.position + " " + Employee.salary + " " + Employee.experience + " " + Employee.city);
+console.log(Employee["name"]);
+console.log(Employee["position"]);
+console.log(Employee["salary"]);
+console.log(Employee["experience"]);
+console.log(Employee["city"]);
